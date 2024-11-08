60c73bd355f65689f79028f7fb174ccbc761d8ba
rutgerkock <114153177+rutgerkock@users.noreply.github.com>
2024-11-08 11:09:30 +0100

    added hints in svg

--- src/lib/components/Circle.svelte
+++ src/lib/components/Circle.svelte
@@ -23,7 +23,7 @@
 
 <Time {updatePercentage} bind:currentTime />
 
-<button class="main-button" popovertarget="my-popover">
+<button popovertarget="my-popover">
     <svg width="600" height="600" viewBox="0 0 600 600" fill="none" xmlns="http://www.w3.org/2000/svg">
         <circle cx="300" cy="300" r={outerRadius} stroke-width="10" />
         <circle cx="300" cy="300" r={innerRadius} stroke-width="10" />
@@ -37,20 +37,44 @@
             transform="rotate(-45 300 300)"
             fill="none" 
         />
-        <text class="time-text" x="300" y="290" text-anchor="middle" dominant-baseline="middle">
+        <text 
+            x="300" 
+            y="290" 
+            text-anchor="middle" 
+            dominant-baseline="middle"
+            font-size="48" 
+            font-family="raleway"
+            font-weight="700"
+            letter-spacing="2">
             {currentTime.slice(0, 5)}
         </text>
-        <text class="label" x="300" y="330" text-anchor="middle" dominant-baseline="middle">
+        <text 
+            class="text-1"
+            x="300" 
+            y="330" 
+            text-anchor="middle" 
+            dominant-baseline="middle"
+            font-size="24" 
+            font-family="raleway"
+            font-weight="300">        
             Fullscreen
         </text>
-        <text class="location" x="300" y="330" text-anchor="middle" dominant-baseline="middle">
+        <text 
+            class="text-2"
+            x="300" 
+            y="330" 
+            text-anchor="middle" 
+            dominant-baseline="middle"
+            font-size="24" 
+            font-family="raleway"
+            font-weight="300">        
             Utrecht, NL
         </text>
     </svg>
 </button>
 
-<button class="popover-button" popover id="my-popover" popovertarget="my-popover">
-    <div class="popover-content">
+<button popover id="my-popover" popovertarget="my-popover">
+    <div>
         <ul>
             <li><h3><Date formatType="fullDate"/></h3></li>
             <li><h3><Weather /></h3></li>
@@ -68,13 +92,37 @@
                 transform="rotate(-45 300 300)"
                 fill="none" 
             />
-            <text class="time-text" x="300" y="290" text-anchor="middle" dominant-baseline="middle">
+            <text 
+                x="300" 
+                y="290" 
+                text-anchor="middle" 
+                dominant-baseline="middle"
+                font-size="48" 
+                font-family="raleway"
+                font-weight="700"
+                letter-spacing="2">
                 {currentTime}
             </text>
-            <text class="label" x="300" y="330" text-anchor="middle" dominant-baseline="middle">
+            <text 
+                class="text-1"
+                x="300" 
+                y="330" 
+                text-anchor="middle" 
+                dominant-baseline="middle"
+                font-size="24" 
+                font-family="raleway"
+                font-weight="300">        
                 Close
             </text>
-            <text class="location" x="300" y="330" text-anchor="middle" dominant-baseline="middle">
+            <text 
+                class="text-2"
+                x="300" 
+                y="330" 
+                text-anchor="middle" 
+                dominant-baseline="middle"
+                font-size="24" 
+                font-family="raleway"
+                font-weight="300">        
                 Utrecht, NL
             </text>
         </svg>
@@ -82,29 +130,42 @@
 </button>
 
 <style>
-    .main-button {
+    button:first-of-type {
         position: absolute;
         right: -12vw;
         margin-top: 22rem;
         width: 40vw;
         height: 40vw;
+    }
+
+    button {
         background: none;
         border: none;
         cursor: pointer;
     }
 
-    .popover-button {
+    .text-1 {
+        visibility: hidden;
+    }
+
+    svg:hover .text-1 {
+        visibility: visible;
+    }
+
+    svg:hover .text-2 {
+        visibility: hidden;
+    }
+
+    button:nth-of-type(2) {
         width: 100vw;
         height: 100vh;
         position: fixed;
         left: 0;
         top: 0;
         background-color: var(--background);
-        border: none;
-        cursor: pointer;
     }
-
-    .popover-content {
+    
+    button:nth-of-type(2) div {
         display: flex;
         flex-direction: row;
         align-items: center;
@@ -112,45 +173,21 @@
         gap: var(--padding-5);
     }
 
-    .time-text {
-        font-size: 48px;
-        font-family: raleway;
-        font-weight: 700;
-        letter-spacing: 2px;
-    }
-
-    .label,
-    .location {
-        font-size: 24px;
-        font-family: raleway;
-        font-weight: 300;
-    }
-
-    .label {
-        visibility: hidden;
-    }
-
-    .popover-button:hover .label {
-        visibility: visible;
-    }
-
-    .popover-button:hover .location {
-        visibility: hidden;
-    }
-
-    .popover-content ul {
+    button ul {
         list-style: none;
         padding: 0;
         margin: 0;
+        display: block;
         text-align: left;
     }
 
-    .popover-content li {
+    button ul li {
         padding: var(--padding-1) 0;
     }
 
+
     @media (prefers-color-scheme: dark) {
-        .popover-button {
+        button:nth-of-type(2) {
             background-color: var(--background-dark);
         }
     }
