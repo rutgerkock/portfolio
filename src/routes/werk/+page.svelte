04eb01cfbaa8380e94501254685940e05f59c7ea
rutgerkock <114153177+rutgerkock@users.noreply.github.com>
2024-11-10 19:27:03 +0100

    poging werk pagina

--- src/routes/werk/+page.svelte
+++ src/routes/werk/+page.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <main class="werk">
-    <h1>Werk</h1>
+    <h1 class="title">Werk</h1>
     <Circle />
     <Nav 
         link1={{ href: '/', name: 'Home' }} 
@@ -12,19 +12,34 @@
     />
     <section>
         <ul class="werkList">
-            <li><p>Project</p></li>
+            <li>
+                <button popovertarget="my-popover-1">
+                    <p>Project</p>
+                </button>
+            </li>
             <li><p>Project</p></li>
             <li><p>Project</p></li>
             <li><p>Project</p></li>
             <li><p>Project</p></li>
         </ul>
+        <button popovertarget="my-popover-1">
+        <div popover id="my-popover-1">
+            <p>Bieb in Bloei</p>
+            <p>De Buurtcampus is een dynamische, laagdrempelige plek waar iedereen zich 
+                welkom en uitgenodigd voelt om kennis op te doen, te ontwikkelen en te delen. 
+                Op het platform Bieb in Bloei krijgen deze projecten een gezicht.
+            </p>
+            <img src="./bieb-in-bloei-1.png" alt="Bieb in bloei afbeelding 2">
+            <img src="./bieb-in-bloei-2.png" alt="Bieb in bloei afbeelding 2">
+        </div>
+        </button>
     </section>
 </main>
 
 <style>
     main {
         padding: var(--padding-2);
-        flex-direction: column;
+        flex-direction: column; 
         background-color: var(--highlight);
     }
 
@@ -33,7 +48,13 @@
         z-index: 998;
     }
 
-    h1 {
+    button {
+        border: none;
+        background-color: transparent;
+        text-align: left;
+    }
+
+    .title {
         opacity: 0;
         animation: reveal 1s 0.2s cubic-bezier(0.075, 0.82, 0.165, 1) forwards;
         text-align: left;
@@ -50,9 +71,36 @@
         line-height: 1;
     }
 
+    div {
+        background-color: var(--highlight);
+        border: none;
+        padding: var(--padding-2);
+        width: 100vw;
+        max-height: 100vh;
+        overflow-y: auto;
+        opacity: 0;
+        transform: translateX(2-0px);
+        animation: popoverReveal 1s cubic-bezier(0.075, 0.82, 0.165, 1) forwards;
+    }
+
+    div p:first-of-type {
+        font-size: var(--title-regular);
+    }
+
+    div p {
+        font-size: var(--text-big);
+        padding-top: var(--padding-3);
+        padding-bottom: var(--padding-2);
+    }
+
+    div img {
+        width: 100%;
+    }
+
     .werkList li {
         opacity: 0;
         animation: reveal 1s cubic-bezier(0.075, 0.82, 0.165, 1) forwards;
+        position: relative;
     }
 
     .werkList li:nth-child(1) {animation-delay: 0.45s;}
@@ -76,6 +124,28 @@
             opacity: 1;
             letter-spacing: 0;
             margin-right: 0;
+        }
+    }
+
+    @keyframes popoverReveal {
+        0% {
+            opacity: 0;
+            transform: translateX(-20px);
+        }
+        100% {
+            opacity: 1;
+            transform: translateX(0);
+        }
+    }
+
+    @media (min-width: 768px) {
+        div {
+            background-color: var(--highlight);
+            border: none;
+            padding: var(--padding-2);
+            width: 40vw;
+            min-width: 30rem;
+            margin-left: 20rem;
         }
     }
 </style>
