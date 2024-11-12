e19d5dbff9edb04303d1655fcc4531247ae16dc4
rutgerkock <114153177+rutgerkock@users.noreply.github.com>
2024-11-12 10:51:57 +0100

    fixed duplicate projects

--- src/lib/components/Projects.js
+++ src/lib/components/Projects.js
@@ -4,6 +4,6 @@ export const projects = [
         description: 'De Buurtcampus is een dynamische, laagdrempelige plek waar iedereen zich welkom en uitgenodigd voelt om kennis op te doen, te ontwikkelen en te delen.',
         images: ['/biebinbloei1.png', '/biebinbloei2.png', '/biebinbloei3.png']
     },
-    { id: 'BiebInBloei', title: 'Bieb in Bloei', description: 'Beschijving' },
-    { id: 'BiebInBloei', title: 'Bieb in Bloei', description: 'Beschijving' },
+    { id: '2', title: 'Project 2', description: 'Beschijving', images: [] },
+    { id: '3', title: 'Project 3', description: 'Beschijving', images: [] },
 ];
